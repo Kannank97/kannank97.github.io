b6ea05e243a6d1741571bca68edcaf0f2b7c2b4e
kannank97 <kannan.valmutty@gmail.com>
2021-05-02 14:01:43 +0530

    ready for public

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const main = document.getElementById('main');
 let parts = [];
 
 let images = [
-  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+  "assets/first.png",
+  "assets/skills.png",
+  "assets/3rd.png"
 ];
 let current = 0;
 let playing = false;
